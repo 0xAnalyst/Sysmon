87c73e37c509efa457683f87a57f02b87e4c2e16
0xpwntester <alibnh1@gmail.com>
2017-11-06 10:23:00 +0300

    Update startupscript.bat

--- startupscript.bat
+++ startupscript.bat
@@ -3,7 +3,7 @@
 SET Configfile="c:\windows\sysmonconfig.xml"
 IF EXIST %Configfile% GOTO END
 
-REM you can use sysmon -u in here for future version for it to unistall the old version before installing sysmon
+REM you can use sysmon -u in here for future versions to unistall the old version before installing new one
 copy /z /y "\\UNCPATH\sysmonconfig.xml" "C:\windows\"
 copy /z /y "\\UNCPATH\sysmon.exe" "C:\windows\"
 
